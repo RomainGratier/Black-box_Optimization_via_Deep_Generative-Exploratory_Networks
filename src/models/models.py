3d6de6a8ca5f674d245d4e9b7a4cff22d80967f4
Romain <4romain@Mac-ROMAIN.local>
2020-04-23 18:27:58 +0200

    update the metrics and the models

--- src/models/models.py
+++ src/models/models.py
@@ -13,76 +13,6 @@ channels=1
 
 img_shape = (channels, img_size, img_size)
 
-class GeneratorDCGAN(nn.Module):
-    # initializers
-    def __init__(self, d=128):
-        super(GeneratorDCGAN, self).__init__()
-
-        self.deconv1 = nn.ConvTranspose2d(101, d*4, 4, 1, 0)
-        self.deconv1_bn = nn.BatchNorm2d(d*4)
-        self.deconv2 = nn.ConvTranspose2d(d*4, d*2, 4, 2, 1)
-        self.deconv2_bn = nn.BatchNorm2d(d*2)
-        self.deconv3 = nn.ConvTranspose2d(d*2, d, 4, 2, 1)
-        self.deconv3_bn = nn.BatchNorm2d(d)
-        self.deconv4 = nn.ConvTranspose2d(d, 1, 4, 2, 3)
-
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
-
-    # forward method
-    def forward(self, input, label):
-        x = torch.cat([input, label],1)
-        x = F.relu(self.deconv1_bn(self.deconv1(x)))
-        x = F.relu(self.deconv2_bn(self.deconv2(x)))
-        x = F.relu(self.deconv3_bn(self.deconv3(x)))
-        x = F.tanh(self.deconv4(x))
-        return x
-
-class DiscriminatorDCGAN(nn.Module):
-    # initializers
-    def __init__(self, d=128):
-        super(DiscriminatorDCGAN, self).__init__()
-        
-        self.conv1_1 = nn.Conv2d(1, int(d/2), 4, 2, 1)
-        self.conv1_2 = nn.Conv2d(1, int(d/2), 4, 2, 1)
-        self.conv2 = nn.Conv2d(d, d*2, 4, 2, 1)
-        self.conv2_bn = nn.BatchNorm2d(d*2)
-        self.conv3 = nn.Conv2d(d*2, d*4, 4, 2, 1)
-        self.conv3_bn = nn.BatchNorm2d(d*4)
-        self.conv4 = nn.Conv2d(d * 4, 1, 3, 1, 0)
-
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
-
-    # forward method
-    def forward(self, input, label):
-        x = F.leaky_relu(self.conv1_1(input), 0.2)
-        y = F.leaky_relu(self.conv1_2(label), 0.2)
-        x = torch.cat([x, y], 1)
-        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
-        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
-        x = F.sigmoid(self.conv4(x))
-
-        return x
-
-def normal_init(m, mean, std):
-    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
-        m.weight.data.normal_(mean, std)
-        m.bias.data.zero_()
-
-def weights_init_normal(m):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(m.bias.data, 0.0)
-
-
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
@@ -179,13 +109,13 @@ class LeNet5(nn.Module):
         output = output.view(img.size(0), -1)
         output = self.fc[1](self.fc[0](output))
         return output
-    
+
 class RMSELoss(nn.Module):
     def __init__(self, eps=1e-6):
         super().__init__()
         self.mse = nn.MSELoss()
         self.eps = eps
-        
+
     def forward(self,yhat,y):
         loss = torch.sqrt(self.mse(yhat,y) + self.eps)
         return loss
