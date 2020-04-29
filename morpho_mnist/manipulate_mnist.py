f348c823d3b68007e3a05594679cfa4d0e30289c
Romain <4romain@Mac-ROMAIN.local>
2020-04-29 11:39:45 +0200

    Added scripts to create new datasets

---
+++ morpho_mnist/manipulate_mnist.py
@@ -0,0 +1,65 @@
+import multiprocessing
+import os
+import shutil
+
+import numpy as np
+
+from morphomnist import io, perturb
+from morphomnist.morpho import ImageMorphology
+
+THRESHOLD = .5
+UP_FACTOR = 4
+
+PERTURBATIONS = [
+    perturb.Thickening(0.5),
+    perturb.Thickening(1.0),
+    perturb.Thickening(1.5),
+    perturb.Thickening(2.0),
+]
+
+
+def process_image(args):
+    i, img = args
+    np.random.seed()
+    morph = ImageMorphology(img, THRESHOLD, UP_FACTOR)
+    out_imgs = [morph.downscale(morph.binary_image)] + \
+               [morph.downscale(pert(morph)) for pert in PERTURBATIONS]
+    return out_imgs
+
+raw_dir = "../Black-box_Optimization_via_Deep_Generative-Exploratory_Networks/data/morpho_mnist/original"
+dataset_root = "../Black-box_Optimization_via_Deep_Generative-Exploratory_Networks/data/processed"
+dataset_names = ["thickened05", "thickened10", "thickened15", "thickened20"]
+
+pool = multiprocessing.Pool()
+for subset in ["train", "t10k"]:
+
+    # Get data pathes
+    imgs_filename = f"{subset}-images-idx3-ubyte.gz"
+    labels_filename = f"{subset}-labels-idx1-ubyte.gz"
+    raw_imgs = io.load_idx(os.path.join(raw_dir, imgs_filename))
+
+    # Multiprocessing for the image perturbation
+    gen = pool.imap(process_image, enumerate(raw_imgs), chunksize=100)
+    try:
+        import tqdm
+        gen = tqdm.tqdm(gen, total=len(raw_imgs), unit='img', ascii=True)
+    except ImportError:
+        def plain_progress(g):
+            print(f"\rProcessing images: 0/{len(raw_imgs)}", end='')
+            for i, res in enumerate(g):
+                print(f"\rProcessing images: {i + 1}/{len(raw_imgs)}", end='')
+                yield res
+            print()
+        gen = plain_progress(gen)
+
+    # Save the results
+    result = zip(*list(gen))
+    for dataset_name, imgs in zip(dataset_names, result):
+        imgs = np.array(imgs)
+        dataset_dir = os.path.join(dataset_root, dataset_name)
+        os.makedirs(dataset_dir, exist_ok=True)
+        io.save_idx(imgs, os.path.join(dataset_dir, imgs_filename))
+        shutil.copy(os.path.join(raw_dir, labels_filename), dataset_dir)
+
+pool.close()
+pool.join()
---
+++ morpho_mnist/measure_new_dataset.py
@@ -0,0 +1,33 @@
+import multiprocessing
+import os
+
+from morphomnist import io, measure
+
+
+def measure_dir(data_dir, pool):
+    for name in ['t10k', 'train']:
+        in_path = os.path.join(data_dir, name + "-images-idx3-ubyte.gz")
+        out_path = os.path.join(data_dir, name + "-morpho.csv")
+        print(f"Processing MNIST data file {in_path}...")
+        data = io.load_idx(in_path)
+        df = measure.measure_batch(data, pool=pool, chunksize=100)
+        df.to_csv(out_path, index_label='index')
+        print(f"Morphometrics saved to {out_path}")
+
+
+def main(data_dirs):
+    with multiprocessing.Pool() as pool:
+        for data_dir in data_dirs:
+            measure_dir(data_dir, pool)
+
+
+if __name__ == '__main__':
+    ROOT = '../Black-box_Optimization_via_Deep_Generative-Exploratory_Networks/data/processed/'
+    dirs = ['thickened05', 'thickened10', 'thickened15', 'thickened20']
+    datadir = [os.path.join(ROOT, dir) for dir in dirs]
+
+    print(f"Processing the following directories :\n{datadir}")
+
+    assert all(os.path.exists(data_dir) for data_dir in datadir)
+
+    main(datadir)
