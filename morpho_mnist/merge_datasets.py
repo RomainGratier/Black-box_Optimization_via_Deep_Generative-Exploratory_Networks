390d22dff52490f5339d76062677d2e73f1c0fb2
Romain <4romain@Mac-ROMAIN.local>
2020-05-01 10:44:00 +0200

    clean the dataset manipulation pipeline

--- morpho_mnist/merge_datasets.py
+++ morpho_mnist/merge_datasets.py
@@ -1,61 +1,14 @@
 import numpy as np
 import pandas as pd
-import gzip
-import struct
 from glob import glob
 import os
+
+from morphomnist import io
 
 def find_data(dirs):
     ''' glob the different data from the main path '''
     data = [[path for path in glob(os.path.join(path_i,'*.*'))] for path_i in dirs]
     return data
-
-def _load_uint8(f):
-    idx_dtype, ndim = struct.unpack('BBBB', f.read(4))[2:]
-    shape = struct.unpack('>' + 'I' * ndim, f.read(4 * ndim))
-    buffer_length = int(np.prod(shape))
-    data = np.frombuffer(f.read(buffer_length), dtype=np.uint8).reshape(shape)
-    return data
-
-def load_idx(path: str) -> np.ndarray:
-    """Reads an array in IDX format from disk.
-    Parameters
-    ----------
-    path : str
-        Path of the input file. Will uncompress with `gzip` if path ends in '.gz'.
-    Returns
-    -------
-    np.ndarray
-        Output array of dtype ``uint8``.
-    References
-    ----------
-    http://yann.lecun.com/exdb/mnist/
-    """
-    open_fcn = gzip.open if path.endswith('.gz') else open
-    with open_fcn(path, 'rb') as f:
-        return _load_uint8(f)
-
-def _save_uint8(data, f):
-    data = np.asarray(data, dtype=np.uint8)
-    f.write(struct.pack('BBBB', 0, 0, 0x08, data.ndim))
-    f.write(struct.pack('>' + 'I' * data.ndim, *data.shape))
-    f.write(data.tobytes())
-
-def save_idx(data: np.ndarray, path: str):
-    """Writes an array to disk in IDX format.
-    Parameters
-    ----------
-    data : array_like
-        Input array of dtype ``uint8`` (will be coerced if different dtype).
-    path : str
-        Path of the output file. Will compress with `gzip` if path ends in '.gz'.
-    References
-    ----------
-    http://yann.lecun.com/exdb/mnist/
-    """
-    open_fcn = gzip.open if path.endswith('.gz') else open
-    with open_fcn(path, 'wb') as f:
-        _save_uint8(data, f)
 
 def merge_datasets(list_paths, data_type):
     if data_type == 'gz':
@@ -63,7 +16,7 @@ def merge_datasets(list_paths, data_type):
         name = list_paths[0].split('/')[-1]
 
         # Load the data
-        data = [load_idx(path) for path in list_paths]
+        data = [io.load_idx(path) for path in list_paths]
 
         # Merge them
         print(data[0].shape)
@@ -71,7 +24,7 @@ def merge_datasets(list_paths, data_type):
         print(data_merged.shape)
 
         # Save them
-        save_idx(data_merged, os.path.join(OUTPUT_PATH, name))
+        io.save_idx(data_merged, os.path.join(OUTPUT_PATH, name))
 
     if data_type == 'csv':
         # Define the name
--- morpho_mnist/resample_new_dataset.py
+++ morpho_mnist/resample_new_dataset.py
@@ -1,56 +1,8 @@
 import numpy as np
 import pandas as pd
-import gzip
-import struct
 from glob import glob
 import os
-
-def _load_uint8(f):
-    idx_dtype, ndim = struct.unpack('BBBB', f.read(4))[2:]
-    shape = struct.unpack('>' + 'I' * ndim, f.read(4 * ndim))
-    buffer_length = int(np.prod(shape))
-    data = np.frombuffer(f.read(buffer_length), dtype=np.uint8).reshape(shape)
-    return data
-
-def load_idx(path: str) -> np.ndarray:
-    """Reads an array in IDX format from disk.
-    Parameters
-    ----------
-    path : str
-        Path of the input file. Will uncompress with `gzip` if path ends in '.gz'.
-    Returns
-    -------
-    np.ndarray
-        Output array of dtype ``uint8``.
-    References
-    ----------
-    http://yann.lecun.com/exdb/mnist/
-    """
-    open_fcn = gzip.open if path.endswith('.gz') else open
-    with open_fcn(path, 'rb') as f:
-        return _load_uint8(f)
-
-def _save_uint8(data, f):
-    data = np.asarray(data, dtype=np.uint8)
-    f.write(struct.pack('BBBB', 0, 0, 0x08, data.ndim))
-    f.write(struct.pack('>' + 'I' * data.ndim, *data.shape))
-    f.write(data.tobytes())
-
-def save_idx(data: np.ndarray, path: str):
-    """Writes an array to disk in IDX format.
-    Parameters
-    ----------
-    data : array_like
-        Input array of dtype ``uint8`` (will be coerced if different dtype).
-    path : str
-        Path of the output file. Will compress with `gzip` if path ends in '.gz'.
-    References
-    ----------
-    http://yann.lecun.com/exdb/mnist/
-    """
-    open_fcn = gzip.open if path.endswith('.gz') else open
-    with open_fcn(path, 'wb') as f:
-        _save_uint8(data, f)
+from morphomnist import io
 
 def get_data(path):
     df_train = pd.read_csv(os.path.join(INPUT_PATH, 'train-morpho.csv'))
@@ -85,8 +37,8 @@ def load_manipulate_save(input_path, out_path, train_index, test_index):
     for path in train_paths:
         name = path.split('/')[-1]
         if name.split('.')[-1] == 'gz':
-            data_new = load_idx(path)[train_index.values]
-            save_idx(data_new, os.path.join(out_path, name))
+            data_new = io.load_idx(path)[train_index.values]
+            io.save_idx(data_new, os.path.join(out_path, name))
         if name.split('.')[-1] == 'csv':
             data_new = pd.read_csv(path).loc[train_index.values]
             data_new.to_csv(os.path.join(out_path, name), index=False)
@@ -97,8 +49,8 @@ def load_manipulate_save(input_path, out_path, train_index, test_index):
     for path in test_paths:
         name = path.split('/')[-1]
         if name.split('.')[-1] == 'gz':
-            data_new = load_idx(path)[test_index.values]
-            save_idx(data_new, os.path.join(out_path, name))
+            data_new = io.load_idx(path)[test_index.values]
+            io.save_idx(data_new, os.path.join(out_path, name))
         if name.split('.')[-1] == 'csv':
             data_new = pd.read_csv(path).loc[test_index.values]
             data_new.to_csv(os.path.join(out_path, name), index=False)
