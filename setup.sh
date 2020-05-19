3728605e82c761961c965e73ca8e20e47299b4e1
Romain G <38717273+RomainGratier@users.noreply.github.com>
2020-05-19 17:18:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,3 @@
 git clone https://github.com/dccastro/Morpho-MNIST.git
 cp -r Morpho-MNIST/morphomnist src/morphomnist
 rm -rf Morpho-MNIST
-
-# Get models for FID/KID measurement
-git clone https://github.com/abdulfatir/gan-metrics-pytorch.git
-cp -r gan-metrics-pytorch/models src/models
-rm -rf gan-metrics-pytorch
