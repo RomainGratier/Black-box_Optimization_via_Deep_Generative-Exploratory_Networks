87e0a662dfc4533aef64a72b28435e2351928dee
Romain <4romain@Mac-ROMAIN.local>
2020-05-22 13:18:19 +0200

    added new inference plots

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -16,11 +16,13 @@ import torch
 from torch.autograd import Variable
 
 cuda = True if torch.cuda.is_available() else False
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
-bayesian = True
+quantile_rate_uncertainty_policy = 0.5
+quantile_rate_z_gen = 0.9
 
 def save_numpy_arr(path, arr):
     np.save(path, arr)
@@ -32,7 +34,7 @@ def get_truncated_normal(form, mean=0, sd=1, quant=0.8):
     return truncnorm(
         (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd).rvs(form)
 
-def generate_sample_from_GAN(target, z, generator, scaler):
+def generate_sample_from_GAN(target, z, generator):
     # Prepare labels
     z = Variable(FloatTensor(z))
     labels = Variable(FloatTensor(get_truncated_normal(z.shape[0], mean=target, sd=1, quant=0.6)))
@@ -40,19 +42,17 @@ def generate_sample_from_GAN(target, z, generator, scaler):
 
     return images_generated, labels
 
-def se_between_target_and_prediction(target, x, forward, trainset):
+def se_between_target_and_prediction(target, x, forward):
 
     y_labels = np.empty(x.shape[0])
     y_labels.fill(target)
-    if bayesian:
-        # CUDA settings
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        y_pred, epistemic, aleatoric = get_uncertainty_per_batch(forward, F.interpolate(x, size=32), device)
-        return se(y_pred, y_labels), y_pred, epistemic
-
-    else:
+    try:
         y_pred = forward(F.interpolate(x, size=32)).squeeze(1).cpu().detach().numpy()
         return se(y_pred, y_labels), y_pred
+
+    except:
+        y_pred, epistemic, aleatoric = get_uncertainty_per_batch(forward, F.interpolate(x, size=32), device)
+        return se(y_pred, y_labels), y_pred, epistemic
 
 def plots_results(target, forward_pred, forward_pred_train, morpho_pred, morpho_pred_train, se, conditions, testset, images_generated, select_img_label_index, fid_value_gen, kid_value_gen, nrow=2, ncol=4):
 
@@ -85,12 +85,6 @@ def compute_fid_mnist_monte_carlo(fake, target, testset, sample_size):
     folder = 'save_data'
     os.makedirs(folder, exist_ok=True)
 
-    # Measure on trained data
-    test_img_label = pd.DataFrame(np.around(testset.labels).values.tolist(), columns=['label'])
-    random.seed(1)
-    select_img_label_index = random.sample(test_img_label[test_img_label['label']==target].index.values.tolist(), sample_size)
-    image_from_test = testset.x_data.numpy()[select_img_label_index]
-
     path_gen = save_numpy_arr(os.path.join(folder, 'gen_img_in_distribution.npy'), fake)
     path_real = save_numpy_arr(os.path.join(folder, 'image_from_test.npy'), image_from_test)
 
@@ -99,22 +93,25 @@ def compute_fid_mnist_monte_carlo(fake, target, testset, sample_size):
 
 def uncertainty_selection(uncertainty, policy_type='quantile'):
     if policy_type == 'quantile':
-        quantile = np.quantile(uncertainty, 0.5)
+        quantile = np.quantile(uncertainty, quantile_rate_uncertainty_policy)
         new_index = np.argwhere(uncertainty < quantile)
     return new_index.squeeze()
 
 
-def monte_carlo_inference(target, generator, forward, trainset, testset, ncol = 4, nrow =2, sample_number_fid_kid = 300, size=2000):
+def monte_carlo_inference(target, generator, forward, testset, ncol = 4, nrow =2, sample_number_fid_kid = 300, size=2000):
 
     # ------------ Sample z from normal gaussian distribution with a bound ------------
-    z = get_truncated_normal((size, cfg.latent_dim), quant=0.9)
+    z = get_truncated_normal((size, cfg.latent_dim), quant=quantile_rate_z_gen)
 
     # ------------ Generate sample from z and y target ------------
-    images_generated, conditions = generate_sample_from_GAN(target, z, generator, trainset.scaler)
+    images_generated, conditions = generate_sample_from_GAN(target, z, generator)
 
     # ------------ Compute the se between the target and the forward model predictions ------------
-    if bayesian:
-        se_forward, forward_pred, uncertainty = se_between_target_and_prediction(target, images_generated, forward, trainset)
+    try:
+        se_forward, forward_pred = se_between_target_and_prediction(target, images_generated, forward)
+    
+    except:
+        se_forward, forward_pred, uncertainty = se_between_target_and_prediction(target, images_generated, forward)
         
         # ------------ Uncertainty policy ------------
         index_certain = uncertainty_selection(uncertainty.squeeze())
@@ -122,9 +119,6 @@ def monte_carlo_inference(target, generator, forward, trainset, testset, ncol = 
         forward_pred = forward_pred[index_certain]
         images_generated = images_generated[index_certain]
         conditions = conditions[index_certain]
-
-    else:
-        se_forward, forward_pred = se_between_target_and_prediction(target, images_generated, forward, trainset)
 
     # Move variable to cpu
     images_generated = images_generated.squeeze(1).cpu().detach().numpy()
@@ -143,10 +137,10 @@ def monte_carlo_inference(target, generator, forward, trainset, testset, ncol = 
     x_train = Variable(testset.x_data[select_img_label_index].type(FloatTensor))
 
     # ------------ Compute the se between testset and the forward model predictions ------------
-    if bayesian:
-        se_forward_train, forward_pred_train, uncertainty_train = se_between_target_and_prediction(target, x_train, forward, trainset)
-    else:
-        se_forward_train, forward_pred_train = se_between_target_and_prediction(target, x_train, forward, trainset)
+    try:
+        se_forward_train, forward_pred_train = se_between_target_and_prediction(target, x_train, forward)
+    except:
+        se_forward_train, forward_pred_train, uncertainty_train = se_between_target_and_prediction(target, x_train, forward)
 
     # ------------ EDA of the best x* generated ------------
     
@@ -205,7 +199,7 @@ def compute_fid_mnist(gen_img, index_distribution, real_dataset, sample_size):
 
     return fid_value, kid_value
 
-def monte_carlo_inference_general(distribution, generator, forward, testset, ncol = 4, nrow =2, sample_number_fid_kid = 300, size=2000):
+def monte_carlo_inference_general(distribution, generator, forward, testset, ncol = 8, nrow =4, sample_number_fid_kid = 300, size=2000):
     
     if distribution == 'in':
         conditions = np.random.uniform(cfg.min_dataset, cfg.limit_dataset, size)
@@ -226,15 +220,16 @@ def monte_carlo_inference_general(distribution, generator, forward, testset, nco
         real_dataset = deepcopy(testset.x_data)
 
     # ------------ Sample z from normal gaussian distribution with a bound ------------
-    z = get_truncated_normal((size, cfg.latent_dim), quant=0.9)
+    z = get_truncated_normal((size, cfg.latent_dim), quant=quantile_rate_z_gen)
 
     # ------------ Generate sample from z and y target ------------
     images_generated = generate_sample_from_GAN__(conditions, z, generator)
 
     # ------------ Compute the se between the target and the forward model predictions ------------
-    if bayesian:
-        # CUDA settings
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    try:
+        y_pred = forward(F.interpolate(images_generated, size=32)).squeeze(1).cpu().detach().numpy()
+    
+    except:
         y_pred, epistemic, aleatoric = get_uncertainty_per_batch(forward, F.interpolate(images_generated, size=32), device)
         
         # ------------ Uncertainty policy ------------
@@ -242,9 +237,6 @@ def monte_carlo_inference_general(distribution, generator, forward, testset, nco
         y_pred = y_pred[index_certain]
         images_generated = images_generated[index_certain]
         conditions = conditions[index_certain]
-
-    else:
-        y_pred = forward(F.interpolate(images_generated, size=32)).squeeze(1).cpu().detach().numpy()
     
     # ------------ Compute FID/KID from testset ------------
     fid_value_gen, kid_value_gen = compute_fid_mnist(images_generated, index_distribution, real_dataset, size)
@@ -261,3 +253,59 @@ def monte_carlo_inference_general(distribution, generator, forward, testset, nco
     print(f"{distribution} distribution results")
     print(f"The mean squared error : {np.mean(se_measure)} \t The std of the squared error : {np.std(se_measure)}")
     print(f"Mean FID : {np.mean(fid_value_gen)} \t Mean KID : {np.mean(kid_value_gen)}")
+    
+    plots_some_results(distribution, images_generated, conditions, y_pred, testset, index_distribution, forward, fid_value_gen, kid_value_gen, nrow=4, ncol=8)
+
+
+def plots_some_results(distribution, images_generated, conditions, forward_pred, testset, index_distribution, forward, fid_value_gen, kid_value_gen, nrow=4, ncol=8):
+    
+    # Gan img selection
+    random_gan_index = random.sample(np.arange(images_generated.size[0]), ncol*nrow)
+    images_generated = images_generated[random_gan_index]
+    conditions = conditions[random_gan_index]
+    forward_pred = forward_pred[random_gan_index]
+    
+    # Real img selection
+    random_id = random.sample(index_distribution.tolist(), ncol*nrow)
+    real_imgs = testset.x_data[random_id].numpy()
+    labels = testset.y_data[random_id].numpy()
+    
+    try:
+        real_pred = forward(F.interpolate(real_imgs, size=32)).squeeze(1).cpu().detach().numpy()
+        bayesian=False
+    except:
+        real_pred, epistemic, aleatoric = get_uncertainty_per_batch(forward, F.interpolate(real_imgs, size=32), device)
+        bayesian=True
+
+    fig, ax = plt.subplots(nrows=nrow, ncols=ncol, figsize=(8,3), dpi=200)
+    
+    for i, row in enumerate(ax):
+        if i < 2:
+            imgs = images_generated
+        else:
+            imgs = real_imgs
+        
+        for j, col in enumerate(row):
+            if i%2 == 0:
+                k = 0
+            else:
+                k = ncol -1
+            if i < 2:
+                image = imgs[j + k]
+                col.imshow(image)
+                col.axis('off')
+                if bayesian:
+                    col.set_title(f"Forward={np.round(float(forward_pred[j + k][0]),1)} / Cond={np.round(float(conditions[j + k]),1)} / Uncertainty={np.round(float(forward_pred[j + k][1]),4)}", fontsize=6) #/ true={np.round(float(morpho_pred[n_top_index[j + k]]),1)} 
+                else:
+                    col.set_title(f"Forward={np.round(float(forward_pred[j + k]),1)} / Cond={np.round(float(conditions[j + k]),1)}", fontsize=6) #/ true={np.round(float(morpho_pred[n_top_index[j + k]]),1)} 
+            else:
+                image = imgs[j + k]
+                col.imshow(image)
+                col.axis('off')
+                if bayesian:
+                    col.set_title(f"Forward={np.round(float(real_pred[j + k]),1)} / Label={np.round(float(labels[j + k]),1)} / Uncertainty={np.round(float(epistemic[j + k]),4)}", fontsize=6) #/ true={np.round(float(morpho_pred[n_top_index[j + k]]),1)} 
+                else:
+                    col.set_title(f"Forward={np.round(float(forward_pred[j + k]),1)} / Label={np.round(float(labels[j + k]),1)}", fontsize=6) #/ true={np.round(float(morpho_pred[n_top_index[j]]),1)}
+    
+    plt.suptitle(f"{distribution} distribution / FID Value : {np.round(fid_value_gen[0])} ± {np.round(fid_value_gen[1])} \ KID Value : {np.around(kid_value_gen[0], decimals=3)}  ± {np.around(kid_value_gen[1], decimals=3)}", fontsize=9)
+    plt.show()
