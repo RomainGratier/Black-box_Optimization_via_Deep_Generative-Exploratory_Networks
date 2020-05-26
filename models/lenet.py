2bd8d29f13227b3bb84b4f6015479706a7ebc394
Romain <4romain@Mac-ROMAIN.local>
2020-05-26 12:40:36 +0200

    added the fid model trained on resampled

---
+++ models/lenet.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+from collections import OrderedDict
+
+class LeNet5(nn.Module):
+    
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        self.convnet = nn.Sequential(OrderedDict([
+            ('c1', nn.Conv2d(1, 6, kernel_size=(5, 5))),
+            ('tanh1', nn.Tanh()),
+            ('s2', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
+            ('c3', nn.Conv2d(6, 16, kernel_size=(5, 5))),
+            ('tanh3', nn.Tanh()),
+            ('s4', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
+            ('c5', nn.Conv2d(16, 120, kernel_size=(5, 5))),
+            ('tanh5', nn.Tanh())
+        ]))
+
+        self.fc = nn.Sequential(OrderedDict([
+            ('f6', nn.Linear(120, 84)),
+            ('tanh6', nn.Tanh()),
+            ('f7', nn.Linear(84, 10)),
+            ('sig7', nn.LogSoftmax(dim=-1))
+        ]))
+
+    def forward(self, img):
+        output = self.convnet(img)
+        output = output.view(img.size(0), -1)
+        output = self.fc(output)
+        return output
+
+    def extract_features(self, img):
+        output = self.convnet(img)
+        output = output.view(img.size(0), -1)
+        output = self.fc[1](self.fc[0](output))
+        return output
--- src/generative_model/metrics.py
+++ src/generative_model/metrics.py
@@ -48,7 +48,8 @@ from collections import OrderedDict
 import src.config as cfg
 from src.generative_model import LeNet5
 
-mnist_model = torch.load(cfg.model_fidkid_path)
+mnist_model = LeNet5()
+mnist_model.load_state_dict(torch.load('models/lenet.pth'))
 if cuda:
     mnist_model.cuda()
 
