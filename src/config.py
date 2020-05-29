7ae211dea51884de74baca2565eadd863f40517b
Romain <4romain@Mac-ROMAIN.local>
2020-05-29 11:43:34 +0200

    fixed minor bugs

--- src/config.py
+++ src/config.py
@@ -1,6 +1,11 @@
 # --------------------- Dataset ---------------------
 experiment = 'min_mnist'
 model_fidkid_path = 'Black-box_Optimization_via_Deep_Generative-Exploratory_Networks/models/lenet.pth'
+data_path = 'processed/original_thic_resample'
+if experiment == 'max_mnist':
+    models_path = '/content/drive/My Drive/master_thesis/models_inference_max/'
+if experiment == 'min_mnist':
+    models_path = '/content/drive/My Drive/master_thesis/models_inference_min/'
 img_size=28
 channels=1
 img_shape = (channels, img_size, img_size)
