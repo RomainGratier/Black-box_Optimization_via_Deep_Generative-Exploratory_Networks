608233034ab0f2a4334418942daf7b9ea286c66e
Romain <4romain@Mac-ROMAIN.local>
2020-05-31 15:36:25 +0200

    fixed some minor bugs

--- src/inference/__init__.py
+++ src/inference/__init__.py
@@ -1,1 +1,1 @@
-from .inference import monte_carlo_inference_general, monte_carlo_inference_fid_kid, monte_carlo_inference_mse
+from .inference import monte_carlo_inference_general, monte_carlo_inference_fid_kid, monte_carlo_inference_fid_kid_batch, monte_carlo_inference_mse
