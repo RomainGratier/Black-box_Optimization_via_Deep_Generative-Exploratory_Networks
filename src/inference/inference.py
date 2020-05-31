a6cda93a325302f27ebca88d73c56e60854c4209
Romain <4romain@Mac-ROMAIN.local>
2020-05-31 10:37:04 +0200

    update the inference pipeline

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -73,7 +73,7 @@ def compute_fid_mnist(gen_img, index_distribution, real_dataset, sample_size):
 
 def compute_global_measures(distribution, images_generated, index_distribution, real_dataset, conditions, forward, size):
 
-    # ------------ Compute Global FID/KID from testset ------------
+    # ------------ random sample ------------
     size = np.around(images_generated.shape[0] * cfginf.quantile_rate_uncertainty_policy , decimals=0)
     random_index = random.sample(np.arange(images_generated.shape[0]).tolist(), int(size))
     images_generated = images_generated[random_index]
@@ -246,3 +246,165 @@ def monte_carlo_inference_general(distribution, generator, forward, testset, nco
     plots_some_results(distribution, images_generated_pol, conditions, forward_pred_pol, testset, index_distribution, forward, fid_value_gen_pol, kid_value_gen_pol, nrow=4, ncol=8)
 
     return se_ms_glob, re_ms_glob, fid_value_gen_glob, kid_value_gen_glob, se_ms_pol, re_ms_pol, fid_value_gen_pol, kid_value_gen_pol
+
+def monte_carlo_inference_mse(distribution, generator, forward, ncol = 8, nrow =4, size=2000):
+    
+    if distribution == 'in':
+        if cfg.experiment == 'max_mnist':
+            conditions = np.random.uniform(cfg_data.min_dataset, cfg_data.limit_dataset, size)
+        if cfg.experiment == 'min_mnist':
+            conditions = np.random.uniform(cfg_data.limit_dataset , cfg_data.max_dataset , size)
+
+    if distribution == 'out':
+        if cfg.experiment == 'max_mnist':
+            conditions = np.random.uniform(cfg_data.limit_dataset, cfg_data.max_dataset, size)
+        if cfg.experiment == 'min_mnist':
+            conditions = np.random.uniform(cfg_data.min_dataset , cfg_data.limit_dataset , size)
+
+    # ------------ Sample z from normal gaussian distribution with a bound ------------
+    z = get_truncated_normal((size, cfgan.latent_dim), quant=cfginf.quantile_rate_z_gen)
+
+    # ------------ Generate sample from z and y target ------------
+    images_generated = generate_sample_from_GAN(conditions, z, generator)
+
+    # ------------ random sample ------------
+    size = np.around(images_generated.shape[0] * cfginf.quantile_rate_uncertainty_policy , decimals=0)
+    random_index = random.sample(np.arange(images_generated.shape[0]).tolist(), int(size))
+    images_generated_rand = images_generated[random_index]
+    
+    # ------------ Compute the forward predictions ------------
+    try:
+        y_pred = forward(F.interpolate(images_generated_rand, size=32)).squeeze(1).cpu().detach().numpy()
+
+    except:
+        y_pred, epistemic, aleatoric = get_uncertainty_per_batch(forward, F.interpolate(images_generated_rand, size=32), device)
+    
+    # ------------ Compare the forward model and the Measure from morphomnist ------------
+    images_generated_rand = images_generated_rand.squeeze(1).cpu().detach().numpy()
+    thickness = compute_thickness_ground_truth(images_generated_rand)
+    
+    # ------------ Compute the se between the target and the morpho measure predictions ------------
+    se_measure_glob = se(thickness, y_pred)
+    re_measure_glob = re(thickness, y_pred)
+    
+    print(f"{distribution} distribution RANDOM results")
+    print(f"The mean squared error : {np.mean(se_measure_glob)} \t The std of the squared error : {np.std(se_measure_glob)}")
+    print(f"The mean relative error : {np.mean(re_measure_glob)} \t The std of the squared error : {np.std(re_measure_glob)}")
+    
+    # ------------ Compute the se between the target and the forward model predictions ------------
+    try:
+        y_pred = forward(F.interpolate(images_generated, size=32)).squeeze(1).cpu().detach().numpy()
+        
+        # ------------ rondom policy ------------
+        random_index = random.sample(np.arange(images_generated.shape[0]).tolist(), int(size))
+        y_pred = y_pred[random_index]
+        epistemic = epistemic[random_index]
+        images_generated = images_generated[random_index]
+        conditions = conditions[random_index]
+
+    except:
+        y_pred, epistemic, aleatoric = get_uncertainty_per_batch(forward, F.interpolate(images_generated, size=32), device)
+ 
+        # ------------ Uncertainty policy ------------
+        index_certain = uncertainty_selection(epistemic.squeeze())
+        y_pred = y_pred[index_certain]
+        epistemic = epistemic[index_certain]
+        images_generated = images_generated[index_certain]
+        conditions = conditions[index_certain]
+
+    # ------------ Compare the forward model and the Measure from morphomnist ------------
+    # Move variable to cpu
+    images_generated = images_generated.squeeze(1).cpu().detach().numpy()
+    thickness = compute_thickness_ground_truth(images_generated)
+
+    # ------------ Compute the se between the target and the morpho measure predictions ------------
+    se_measure = se(thickness, y_pred)
+    re_measure = re(thickness, y_pred)
+
+    print(f"{distribution} distribution POLICY results")
+    print(f"The mean squared error : {np.mean(se_measure)} \t The std of the squared error : {np.std(se_measure)}")
+    print(f"The mean relative error : {np.mean(re_measure)} \t The std of the squared error : {np.std(re_measure)}")
+    print(f"Mean FID : {fid_value_gen[0]} ± {fid_value_gen[1]} \t Mean KID : {kid_value_gen[0]} ± {kid_value_gen[1]}")
+    
+    # ------------ Global results ------------
+    se_ms_glob = [np.mean(se_measure_glob), np.std(se_measure_glob)]; re_ms_glob = [np.mean(re_measure_glob), np.std(re_measure_glob)];
+    se_ms_pol = [np.mean(se_measure_pol), np.std(se_measure_pol)]; re_ms_pol = [np.mean(re_measure_pol), np.std(re_measure_pol)];
+    
+    return se_ms_glob, re_ms_glob, se_ms_pol, re_ms_pol
+
+def monte_carlo_inference_fid_kid(distribution, generator, forward, testset, ncol = 8, nrow =4, sample_number_fid_kid = 1000, size_sample=50):
+    
+    size = int(sample_number_fid_kid * 1/cfginf.quantile_rate_uncertainty_policy)
+    fid_pol = []; fid_rand = []; kid_pol = []; kid_rand = [];
+    for i in range(size_sample):
+    
+        if distribution == 'in':
+            if cfg.experiment == 'max_mnist':
+                conditions = np.random.uniform(cfg_data.min_dataset, cfg_data.limit_dataset, size)
+                df_test_in = pd.DataFrame(testset.y_data, columns=['label'])
+                index_distribution = df_test_in[df_test_in['label'] <= cfg_data.limit_dataset].index
+                print(f'size of in distribution data for fid/kid : {len(index_distribution)}')
+                real_dataset = deepcopy(testset.x_data)
+            if cfg.experiment == 'min_mnist':
+                conditions = np.random.uniform(cfg_data.limit_dataset , cfg_data.max_dataset , size)
+                df_test_in = pd.DataFrame(testset.y_data, columns=['label'])
+                index_distribution = df_test_in[(df_test_in['label'] > cfg_data.limit_dataset) & df_test_in['label'] <= cfg_data.max_dataset].index
+                print(f'size of in distribution data for fid/kid : {len(index_distribution)}')
+                real_dataset = deepcopy(testset.x_data)
+    
+        if distribution == 'out':
+            if cfg.experiment == 'max_mnist':
+                conditions = np.random.uniform(cfg_data.limit_dataset, cfg_data.max_dataset, size)
+                df_test_out = pd.DataFrame(testset.y_data, columns=['label'])
+                index_distribution = df_test_out[df_test_out['label'] > cfg_data.limit_dataset].index
+                print(f'size of out distribution data for fid/kid : {len(index_distribution)}')
+                real_dataset = deepcopy(testset.x_data)
+            if cfg.experiment == 'min_mnist':
+                conditions = np.random.uniform(cfg_data.min_dataset , cfg_data.limit_dataset , size)
+                df_test_out = pd.DataFrame(testset.y_data, columns=['label'])
+                index_distribution = df_test_out[df_test_out['label'] <= cfg_data.limit_dataset].index
+                print(f'size of out distribution data for fid/kid : {len(index_distribution)}')
+                real_dataset = deepcopy(testset.x_data)
+    
+        # ------------ Sample z from normal gaussian distribution with a bound ------------
+        z = get_truncated_normal((size, cfgan.latent_dim), quant=cfginf.quantile_rate_z_gen)
+    
+        # ------------ Generate sample from z and y target ------------
+        images_generated = generate_sample_from_GAN(conditions, z, generator)
+        
+        # ------------ random sample ------------
+        size = np.around(images_generated.shape[0] * cfginf.quantile_rate_uncertainty_policy , decimals=0)
+        random_index = random.sample(np.arange(images_generated.shape[0]).tolist(), int(size))
+        images_generated_rand = images_generated[random_index]
+    
+        # ------------ Compute FID/KID from testset ------------
+        fid_value_gen_rand, kid_value_gen_rand = compute_fid_mnist(images_generated, index_distribution, real_dataset, size)
+    
+        # ------------ Compute policy measures ------------
+        try:
+            y_pred = forward(F.interpolate(images_generated, size=32)).squeeze(1).cpu().detach().numpy()
+            forward_pred = np.array(y_pred).T
+    
+        except:
+            y_pred, epistemic, aleatoric = get_uncertainty_per_batch(forward, F.interpolate(images_generated, size=32), device)
+     
+            # ------------ Uncertainty policy ------------
+            index_certain = uncertainty_selection(epistemic.squeeze())
+            y_pred = y_pred[index_certain]
+            epistemic = epistemic[index_certain]
+            forward_pred = np.array([y_pred, epistemic.squeeze(1)]).T
+            images_generated = images_generated[index_certain]
+            conditions = conditions[index_certain]
+    
+        # ------------ Compute FID/KID from testset ------------
+        fid_value_gen_pol, kid_value_gen_pol = compute_fid_mnist(images_generated, index_distribution, real_dataset, size)
+        
+        # Update values
+        fid_pol.append(fid_value_gen_pol[0]); fid_rand.append(fid_value_gen_rand[0]); kid_pol.append(kid_value_gen_pol[0]); kid_rand.append(kid_value_gen_rand[0]);
+        
+    stat_fid_rand = [np.mean(fid_rand), np.std(fid_rand)/np.sqrt(size_sample)]
+    stat_fid_pol = [np.mean(fid_pol), np.std(fid_pol)/np.sqrt(size_sample)]
+    stat_kid_rand = [np.mean(kid_rand), np.std(kid_rand)/np.sqrt(size_sample)]
+    stat_kid_pol = [np.mean(kid_pol), np.std(kid_pol)/np.sqrt(size_sample)]
+    
+    return stat_fid_rand, stat_fid_pol, stat_kid_rand, stat_kid_pol
