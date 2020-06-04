d51fc817cdac94730c0b805444a2c473815ba21f
Romain <4romain@Mac-ROMAIN.local>
2020-06-04 20:24:53 +0200

    fixed some bugs

---
+++ src/config_dcgan.py
@@ -0,0 +1,11 @@
+# --------------------- GAN ---------------------
+n_epochs = 50
+lr=0.0005
+b1=0.5
+b2=0.999
+sample_interval=500
+n_row = 10
+fid_kid_sample = 1000
+latent_dim=100
+label_dim_input = 1
+batch_size=128
