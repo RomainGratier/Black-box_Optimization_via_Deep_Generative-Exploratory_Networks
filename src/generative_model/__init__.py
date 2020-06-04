1597f0803cc6bd53887e3db0c7af7cbf526ef9bb
Romain <4romain@Mac-ROMAIN.local>
2020-06-04 12:11:33 +0200

    fixed some bugs

--- src/generative_model/__init__.py
+++ src/generative_model/__init__.py
@@ -1,3 +1,3 @@
 from .models import Generator, Discriminator, LeNet5, LeNet5Regressor, CondDCDiscriminator, CondDCGenerator
-from .train_model import train_gan_model
+from .train_model import train_gan_model, train_wgan_model
 from .create_model_fid_kid import create_model_fid_kid
